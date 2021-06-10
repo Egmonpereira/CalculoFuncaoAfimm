1c9eac717dbb538a7c6c7bd593f44d315ac9a2c3
Egmonpereira <egmonpereira@gmail.com>
2021-06-10 16:33:06 -0300

    Programa funcionando

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-import os 
+import os
+
+def conversor(n):
+    a = ""
+    aux = 0
+    for i in n:
+        if i == "/":
+            b = int(n[aux + 1])
+            break
+        else:
+            a = (a + i)
+        aux = aux + 1
+    try:
+        return int(a)/int(b)
+    except:
+        return int(n)
+
 
 if __name__ == "__main__":
     os.system("clear")
-    x = int(input(Valor de X: ))
-    y = int(input(Valor de Y: ))
-    
-    cx = int(input(Coeficiente de X: ))
-    cy = int(input(Coeficiente de Y: ))
+    n = input('Coeficiente de X: ')
+    cx = conversor(n)
+    n = input('Valor de X: ')
+    x = conversor(n)
+    n = input('Coeficiente de Y: ')
+    cy = conversor(n)
+    n = input('Valor de Y: ')
+    y = conversor(n)
+
+
     
     print(cx * x + cy * y)
